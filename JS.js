e85adcc082f6b1acae51f7ae44e7780fb7201ddf
Matias Segura <matismarinelli@gmail.com>
2022-04-26 17:15:36 -0300

    Tp completo

---
+++ JS.js
@@ -0,0 +1,4 @@
+var principal = document.getElementById('principal')
+var titulo = document.createElement('h1');
+titulo.innerHTML = "Binevenido a esta pagina mi t.p N°3";
+principal.appendChild(titulo)
